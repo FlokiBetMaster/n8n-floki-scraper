cabbd8ce791e6ed0cd8d79623d3a47638c172275
FlokiBetMaster <manugarxiamusic@gmail.com>
2025-04-12 12:05:44 -0500

    Update stake_manager.py

--- stake_manager.py
+++ stake_manager.py
@@ -1,17 +1,10 @@
-def calculate_stake(prob):
-    if prob >= 70:
-        return 2
-    elif prob >= 60:
-        return 3
-    elif prob >= 50:
-        return 4
-    elif prob >= 40:
-        return 5
-    elif prob >= 30:
-        return 6
-    elif prob >= 20:
-        return 7
-    elif prob >= 10:
-        return 8
-    else:
-        return 10
+def calculate_stake(prob, bank=100000):
+    if prob > 50: return 1
+    if prob > 40: return 2
+    if prob > 30: return 3
+    if prob > 20: return 5
+    if prob > 10: return 7
+    return 10
+
+def update_bank(bank, stake):
+    return {"new_bank": bank - stake}
